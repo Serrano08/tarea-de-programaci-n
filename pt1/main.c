172049066915186bacbf6c7e7dd7a0e6f5a4c78f
Serrano08 <157764599+Serrano08@users.noreply.github.com>
2024-02-01 18:37:07 -0400

    Add files via upload

---
+++ pt1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main (void)
+{
+    int A,B,C,D;
+    printf("ingrese cuatro numeros de tipo entero separados por espacio: ");
+    scanf("%d %d %d %d", &A,&B,&C,&D);
+    printf("\n %d %d %d %d \n", D,C,B,A);
+}
